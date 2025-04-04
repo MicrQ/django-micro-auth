1d46c043a8ea3e63e22fe558e58914b10f8ce65d
Abenet Gebre <mail.micrq@proton.me>
2025-04-04 11:27:01 +0300

    prepared setup for packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+""" setup script for the `django-micro-auth` package """
+
+setup(
+    name='django_micro_auth',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Abenet Gebre',
+    author_email='mail.micrq@proton.me',
+    description='A lightweight Django authentication\
+          library with DRF and drf-spectacular support.',
+    url='https://github.com/MicrQ/django-micro-auth',
+    license='MIT'
+)
