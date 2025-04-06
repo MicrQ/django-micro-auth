d62d29fefe4b8119edb096d9ccb0b714be6351e8
Abenet Gebre <mail.micrq@proton.me>
2025-04-06 18:53:14 +0300

    Added url for login

--- django_micro_auth/urls.py
+++ django_micro_auth/urls.py
@@ -1,9 +1,9 @@
 """ definitions of URL patterns for the application """
 from django.urls import path
-from .views import RegisterAPIView
+from .views import RegisterAPIView, LoginAPIView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-
+    path('login/', LoginAPIView.as_view(), name='login')
 ]
