b68508943eb5c9128f47b9b8f714fbb98b88ff5d
Abenet Gebre <mail.micrq@proton.me>
2025-04-22 16:08:38 +0300

    Listed dependencies to be installed with the package.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-
 from setuptools import setup, find_packages
 """ setup script for the `django-micro-auth` package """
 
 setup(
-    name='django_micro_auth',
+    name='django-micro-auth',
     version='0.1.0',
     packages=find_packages(),
     author='Abenet Gebre',
     author_email='mail.micrq@proton.me',
-    description='A lightweight Django authentication\
-          library with DRF and drf-spectacular support.',
+    description='A lightweight Django authentication library with DRF and drf-spectacular support.',
     url='https://github.com/MicrQ/django-micro-auth',
-    license='MIT'
+    license='MIT',
+    install_requires=[
+        "Django>=5",
+        "djangorestframework>=3.16",
+        "drf-spectacular>=0.28",
+    ],
+    python_requires='>=3.8'
 )
